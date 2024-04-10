a1a70571ed5c9ac994b32e1ecc1fdea0896ce500
Krishnat Mahajan <mahajankrishna798@gmail.com>
2024-04-10 18:33:45 +0530

    initial stage in reference.js

---
+++ reference.js
@@ -0,0 +1,8 @@
+function sum(num1, num2) {
+    return num1 + num2;
+  }
+  
+  const num1 = 5;
+  const num2 = 7;
+  const sumOfNumbers = sum(num1, num2);
+  console.log(sumOfNumbers); // Output: 12
